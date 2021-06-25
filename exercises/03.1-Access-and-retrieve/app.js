72d42733a05632c2d99a2b0681f4f2a90cc760b4
cmsosa015 <cmsosa015@gmail.com>
2021-06-25 02:32:22 +0000

    unsaved

--- exercises/03.1-Access-and-retrieve/app.js
+++ exercises/03.1-Access-and-retrieve/app.js
@@ -1,8 +1,12 @@
 //declaring the array
 var myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
 
-//1. print the item here
+//1. print the item here 
+let day = myArray[2];
+console.log(day)
 
 //2. change 'thursday'a value here to null
+ myArray[4]= null;
 
 //3. print the position of step 2
+console.log(myArray[4])
